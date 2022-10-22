4b21db303a89c6b603fd694ee8337be1aa671a91
Joey van der Poel <Joey.van.der.poel@gmail.com>
2022-10-22 12:19:24 +0200

    Begin van een java Syteem dat interesses makkelijk maakt te koppieren

---
+++ out/production/team-4/assets/js/javascript.js
@@ -0,0 +1,40 @@
+class myHeader extends HTMLElement {
+    connectedCallback() {
+        // Any time this element is connected to a document this function will run
+        this.innerHTML = `
+            <header>
+                <a href="../index.html">
+                    <img id="logo" src="img/corendon_logo.png" alt="Corendon logo">
+                </a>
+                <nav>
+                    <ul>
+                        <li><a href="../index.html"><img src="img/home_icon.png" alt="Home icon"></a></li>
+                        <li><a href="profiel.html">Profiel</a></li>
+                        <li><a href="matches.html">Matches</a></li>
+                        <li><a href="potentiële_matches.html">Potentiële matches</a></li>
+                        <li><a href="over.html">Over</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+        `
+    }
+}
+
+// Any time the HTML parser runs across a tag named my-header, it will populate its innerHTML
+customElements.define('my-header', myHeader)
+
+class myFooter extends HTMLElement {
+    connectedCallback() {
+        // Any time this element is connected to a document this function will run
+        this.innerHTML = `
+            <footer>
+                <p><a href="mailto:corendon@example.com">corendon@example.com</a></p>
+                <p>@ 2022 Corendon</p>
+            </footer>
+        `
+    }
+}
+
+// Any time the HTML parser runs across a tag named my-footer, it will populate its innerHTML
+customElements.define('my-footer', myFooter)
