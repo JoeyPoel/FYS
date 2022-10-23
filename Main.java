a7cefd2bbab20f61428622a41019ebedf7422110
Joey van der Poel <Joey.van.der.poel@gmail.com>
2022-10-23 22:07:07 +0200

    Java programma voor interesses werkt nu perfect

--- Main.java
+++ Main.java
@@ -9,26 +9,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         System.out.print("Hoe veel keer wil je een interesse invullen? ");
         int hoeVaak = in.nextInt();
-        for (int i = 0; i <= hoeVaak; i++) {
-            System.out.print("Vul hier de interesse in: ");
-            String input = in.nextLine();
-            System.out.printf("<option value=\"%s\">%s</option>\n\n", input, input);
 
+
+        String[] output = InteresseMethod(hoeVaak) ;
+        for (int i = 0; i < hoeVaak; i++){
+            System.out.println("<option value=\"" + output[i] +  "\">" + output[i] + "</option>");
         }
+    }
 
+    public static String[] InteresseMethod (int hoeVaak){
+        Scanner in = new Scanner(System.in);
+        String[] Interesse = new String[hoeVaak];
+
+        System.out.print("Vul hier de interesse in: ");
+        for (int i = 0; i < hoeVaak; i++){
+            System.out.print("Vul hier de interesse in: ");
+            Interesse[i] = in.nextLine();
+        }
+        return Interesse;
     }
 }
-/*
-    public static String Input (int hoeVaak){
-        Scanner in = new Scanner(System.in);
-        String[] InputArray = new String();
-        for(int i = 0; i <= hoeVaak; i++){
-            System.out.print("Vul hier de interesse in: ");
-            String input = in.nextLine();
-            System.out.printf("<option value=\"%s\">%s</option>\n\n",input,input);
-            InputArray[i] = input.nextFloat();
-        }
-    }
- */
+
+
+
