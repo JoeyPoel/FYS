9415e27fc98a7c02b912676160cc757b59ea59c0
Joey van der Poel <Joey.van.der.poel@gmail.com>
2022-11-11 15:32:18 +0100

    Slides op de main page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -73,3 +73,14 @@ function checkInfo() {
         }
     })
 }
+
+// slides op main page
+
+let counter = 2;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 5000);
