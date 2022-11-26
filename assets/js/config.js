dd989d80201e1506c432b736abe262c78e4a6baf
bakarej <jimi.bakare@hva.nl>
2022-11-26 23:03:38 +0100

    VERBINDING MET DATABASE EN INSERT WERKT OOK!

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,14 @@
 FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is101_4.kQepJlZ8TUMLReYA",
-    database: "fys_is101_4_joeyvanderpoel",
+    database: "fys_is101_4_live",
     environment: "mockup"
+});
+
+FYSCloud.API.queryDatabase(
+    "SELECT * FROM usertype"
+).then(function(data) {
+    console.log(data);
+}).catch(function(reason) {
+    console.log(reason);
 });
