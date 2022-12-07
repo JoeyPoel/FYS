d7bfe5f14ebee9e80401b4db17e09c8657907559
bakarej <jimi.bakare@hva.nl>
2022-12-07 11:08:27 +0100

    Elke pagina heeft zijn eigen JavaScript file, dat is handig en zie je niet in F12 de errors van andere pagina's.

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,3 +1,4 @@
+/*
 FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is101_4.kQepJlZ8TUMLReYA",
@@ -8,25 +9,6 @@ FYSCloud.API.configure({
 let data = await getData();
 document.getElementById("div-test").innerText = data[1].email;
 makeDiv("Hoi Joey");makeDiv("heeeey");
-
-/*FYSCloud.API.queryDatabase(
-    "SELECT * FROM account"
-).then(function(data) {
-    console.log(data);
-}).catch(function(reason) {
-    console.log(reason);
-});*/
-/*function getData() {
-    FYSCloud.API.queryDatabase(
-        "SELECT * FROM account"
-    ).then(function (data) {
-        console.log(data);
-        return data;
-    }).catch(function (reason) {
-        console.error(reason);
-        return null;
-    });
-}*/
 
 async function getData() {
     try {
@@ -48,4 +30,25 @@ function makeDiv(divParameter) {
 
 for (let i = 0; i < data.length; i++) {
     makeDiv(data[i].email);
+} */
+
+// Form testing
+window.addEventListener("load", addListeners);
+
+function addListeners() {
+    document.getElementById("submitBtn").addEventListener("click", formFunction);
+}
+
+function formFunction() {
+    let voornaam;
+    let tussenvoegsel;
+    let achternaam;
+    voornaam = document.getElementById("voornaam").value;
+    tussenvoegsel = document.getElementById("tussenvoegsel").value;
+    achternaam = document.getElementById("achternaam").value;
+
+    document.getElementById("second").innerHTML = "HET WERKT!!!!";
+    console.log(voornaam);
+    console.log(tussenvoegsel);
+    console.log(achternaam);
 }
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,48 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    class myHeader extends HTMLElement {
-        connectedCallback() {
-            // Any time this element is connected to a document this function will run
-            this.innerHTML = `
-                  <div class="Navbar">
-                        <nav>
-                            <ul class="Navbar-button" id="MyNavbar">
-                                <li><a href="../index.html" class="Logo">LOGO</a></li>
-                                <li><a href="login.html" class="button">LOG IN</a></li>
-                                <li><a href="contact.html" class="button" class="Contact">CONTACT</a></li>
-                                <li><a href="../index.html#over" class="button" class="Over">OVER</a</li>
-                                <li><a href="matches.html" class="button" class="Matches">MATCHES</a></li>
-                                <li><a href="potentiële_matches.html" class="button" class="Zoek-Andere-Reizigers">ZOEK ANDERE REIZIGERS</a></li>
-                                <li><a href="profiel.html" class="button" class="Contact"> PROFIEL</a></li>
-                            </ul>
-                        </nav>
-                  </div>
-            `
-        }
-    }
-
-    // Any time the HTML parser runs across a tag named my-header, it will populate its innerHTML
-    customElements.define('my-header', myHeader);
-
-    class myFooter extends HTMLElement {
-        connectedCallback() {
-            // Any time this element is connected to a document this function will run
-            this.innerHTML = `
-                <div class="container-1-footer">
-                    <footer>
-                        <div>
-                            <p><a href="mailto:corendon@example.com">corendon@example.com</a></p>
-                            <p>@ 2022 Corendon</p>
-                        </div>
-                    </footer>
-                </div>
-            `
-        }
-    }
-
-    // Any time the HTML parser runs across a tag named my-footer, it will populate its innerHTML
-    customElements.define('my-footer', myFooter);
-
     //Over pagina scroll
     const boxes = document.querySelectorAll('.info');
 
