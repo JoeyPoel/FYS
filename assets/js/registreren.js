b5afc4d16ccdef92bd403ffd7858d1a30b47d56d
bakarej <jimi.bakare@hva.nl>
2022-12-15 00:33:40 +0100

    Deel 2 gewerkt aan registreren, maar nog niet af.

--- assets/js/registreren.js
+++ assets/js/registreren.js
@@ -6,23 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const passwordInput = document.querySelector('#password');
     const confirmPasswordInput = document.querySelector('#confirm-password');
 
-    form.addEventListener('submit', (event)=>{
-
+    form.addEventListener('submit', (event) => {
         validateForm();
         console.log(isFormValid());
-        if(isFormValid()==true){
+        if (isFormValid() == true) {
             form.submit();
-        }else {
+        } else {
             event.preventDefault();
         }
-
     });
 
-    function isFormValid(){
+    function isFormValid() {
         const inputContainers = form.querySelectorAll('.input-group');
         let result = true;
-        inputContainers.forEach((container)=>{
-            if(container.classList.contains('error')){
+        inputContainers.forEach((container) => {
+            if (container.classList.contains('error')) {
                 result = false;
             }
         });
@@ -31,43 +29,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function validateForm() {
         //USERNAME
-        if(usernameInput.value.trim()==''){
+        if (usernameInput.value.trim() == '') {
             setError(usernameInput, 'Name can not be empty');
-        }else if(usernameInput.value.trim().length <5 || usernameInput.value.trim().length > 15){
+        } else if (usernameInput.value.trim().length < 5 || usernameInput.value.trim().length > 15) {
             setError(usernameInput, 'Name must be min 5 and max 15 charecters');
-        }else {
+        } else {
             setSuccess(usernameInput);
         }
         //EMAIL
-        if(emailInput.value.trim()==''){
+        if (emailInput.value.trim() == '') {
             setError(emailInput, 'Provide email address');
-        }else if(isEmailValid(emailInput.value)){
+        } else if (isEmailValid(emailInput.value)) {
             setSuccess(emailInput);
-        }else{
+        } else {
             setError(emailInput, 'Provide valid email address');
         }
-
         //PASSWORD
-        if(passwordInput.value.trim()==''){
+        if (passwordInput.value.trim() == '') {
             setError(passwordInput, 'Password can not be empty');
-        }else if(passwordInput.value.trim().length <6 || passwordInput.value.trim().length >20){
+        } else if (passwordInput.value.trim().length < 6 || passwordInput.value.trim().length > 20) {
             setError(passwordInput, 'Password min 6 max 20 charecters');
-        }else {
+        } else {
             setSuccess(passwordInput);
         }
         //CONFIRM PASSWORD
-        if(confirmPasswordInput.value.trim()==''){
+        if (confirmPasswordInput.value.trim() == '') {
             setError(confirmPasswordInput, 'Password can not be empty');
-        }else if(confirmPasswordInput.value !== passwordInput.value){
+        } else if (confirmPasswordInput.value !== passwordInput.value) {
             setError(confirmPasswordInput, 'Password does not match');
-        }else {
+        } else {
             setSuccess(confirmPasswordInput);
         }
     }
 
     function setError(element, errorMessage) {
         const parent = element.parentElement;
-        if(parent.classList.contains('success')){
+        if (parent.classList.contains('success')) {
             parent.classList.remove('success');
         }
         parent.classList.add('error');
@@ -75,17 +72,16 @@ document.addEventListener('DOMContentLoaded', () => {
         paragraph.textContent = errorMessage;
     }
 
-    function setSuccess(element){
+    function setSuccess(element) {
         const parent = element.parentElement;
-        if(parent.classList.contains('error')){
+        if (parent.classList.contains('error')) {
             parent.classList.remove('error');
         }
         parent.classList.add('success');
     }
 
-    function isEmailValid(email){
-        const reg =/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
+    function isEmailValid(email) {
+        const reg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
         return reg.test(email);
     }
 });
