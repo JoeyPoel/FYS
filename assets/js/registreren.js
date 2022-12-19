8769f0e5c17e53a0b7241bd06b5edb881d583100
bakarej <jimi.bakare@hva.nl>
2022-12-19 18:34:08 +0100

    LET'S GO! WERKT: Bij registratie wordt gecheckt of gebruikersnaam of email al bestaat. en het werkt.... LET'S GO!

--- assets/js/registreren.js
+++ assets/js/registreren.js
@@ -17,9 +17,19 @@ document.addEventListener('DOMContentLoaded', () => {
         validateForm();
         console.log(isFormValid());
         if (isFormValid() == true) {
-            form.submit();
-            insertData();
-            //getData();
+            event.preventDefault();
+            Promise.all([getData()]).then(values => {
+                console.log(values);
+                if (values[0] == true) {
+                    console.log("FALSE");
+                    alert("Gebruikersnaam of email bestaat al!");
+                    event.preventDefault();
+                } else {
+                    console.log("TRUE");
+                    form.submit();
+                    insertData();
+                }
+            });
         } else {
             event.preventDefault();
         }
@@ -110,12 +120,22 @@ document.addEventListener('DOMContentLoaded', () => {
     // Function with SQL that retrieves data from the database
     async function getData() {
         try {
+            let status = true;
             const data = await FYSCloud.API.queryDatabase(
-                "SELECT idAccount FROM account WHERE gebruikersnaam=?;",
-                [usernameInput.value]
+                "SELECT * FROM fys_is101_4_live.`account` WHERE gebruikersnaam=? OR email=?;",
+                [usernameInput.value, emailInput.value]
             );
-            console.log(data[0].idAccount);
-            return FYSCloud.Session.set(usernameInput.value, data[0].idAccount);
+            // if statement that checks if variable data has any values
+            if (!data.length) {
+                console.log("No data!");
+                status = false;
+            } else { // else statement for when variable data has values
+                console.log("There is data.");
+                console.log(data[0].idAccount);
+                //console.log(data.length);
+                //FYSCloud.Session.set(emailInput.value, data[0].idAccount);
+            }
+            return status;
         } catch (error) {
             return null;
         }
